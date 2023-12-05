504d7fce5b294527865db6d42e865469db67daf3
Andrew Lewis <andrew.lewis@appfolio.com>
2023-12-04 21:39:16 -0800

    Create template file

---
+++ template.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+SAMPLE_INPUT =
+"
+"
+
+MY_INPUT =
+"
+"
+
+# AoC DayX
+class DayX
+  def initialize
+
+  end
+
+  def compute(input, part2: false)
+
+  end
+
+  private
+
+end
+
+day = DayX.new
+
+p day.compute(SAMPLE_INPUT)
+# p day.compute(MY_INPUT)
+
+# p day.compute(SAMPLE_INPUT, part2: true)
+# p day.compute(MY_INPUT, part2: true)
