57443baf1608b20dc85c385172e2fe57c47af2a7
Andrew Lewis <andrew.lewis@appfolio.com>
2023-12-07 22:36:28 -0800

    D8P2

--- day8.rb
+++ day8.rb
@@ -11,15 +11,32 @@ class Day8
     @nodes = {}
     directions = clean_input(input)
 
-    Kernel.loop do
-      directions.chars.each do |direction|
-        count += 1
-        pointer = direction == 'L' ? @nodes[pointer][0] : @nodes[pointer][1]
+    unless part2
+      Kernel.loop do
+        directions.chars.each do |direction|
+          count += 1
+          pointer = direction == 'L' ? @nodes[pointer][0] : @nodes[pointer][1]
+          break if pointer == 'ZZZ'
+        end
         break if pointer == 'ZZZ'
       end
-      break if pointer == 'ZZZ'
     end
-    count
+
+    if part2
+      origin_pointers = @nodes.select { |k, _| k[-1] == 'A' }.keys
+      pointers = origin_pointers
+      checkpoints = []
+      Kernel.loop do
+        directions.chars.each do |direction|
+          count += 1
+          pointers.map! { |point| direction == 'L' ? @nodes[point][0] : @nodes[point][1] }
+          pointers.each_with_index { |point, i| checkpoints[i] = count if point[-1] == 'Z' && checkpoints[i].nil? }
+          break if checkpoints.compact.length == origin_pointers.length
+        end
+        break if checkpoints.compact.length == origin_pointers.length
+      end
+    end
+    checkpoints.reduce(1, :lcm)
   end
 
   private
@@ -27,7 +44,7 @@ class Day8
   def clean_input(input)
     lines = input.split("\n")
     directions = lines.shift
-    maps = lines[1..].map { |line| line.scan(/[A-Z]+/) }
+    maps = lines[1..].map { |line| line.scan(/[^=)(, ]+/) }
     maps.each do |map|
       @nodes[map[0]] = [map[1], map[2]]
     end
@@ -38,7 +55,7 @@ end
 day = Day8.new
 
 # p day.compute(SAMPLE)
-p day.compute(MAIN)
+# p day.compute(MAIN)
 
-# p day.compute(SAMPLE, part2: true)
-# p day.compute(MAIN, part2: true)
+# p day.compute(SAMPLE2, part2: true)
+p day.compute(MAIN, part2: true)
